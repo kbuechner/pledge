8a2c60faba14c014cce52787a41e3d36591fe25b
Katherine Buechner <kbuechner@gmail.com>
2016-06-27 11:54:49 -0400

    noon

--- pledge.js
+++ pledge.js
@@ -4,10 +4,65 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise(){
+	this.state = 'pending';
+	this.value = null;
+	this.handlerGroups = [];
+}
+
+$Promise.prototype.then = function(success, error){
+	var handler = {
+		successCb: null,
+		errorCb: null
+	}
+
+	if(typeof success == 'function'){
+		handler.successCb = success;
+
+	}
+
+	if(typeof error == 'function'){
+		handler.errorCb = error;
+	}
+
+	this.handlerGroups.push(handler);
+}
+
+$Promise.prototype.callHandlers = function(){
+	console.log(this)
+
+}
 
 
+function Deferral(){
+	this.$promise = new $Promise;
+}
+
+Deferral.prototype.resolve = function(resolveData){
+
+	if(this.$promise.state == 'pending'){
+		this.$promise.state = 'resolved';
+		this.$promise.value = resolveData;
+	}	
+}
+
+Deferral.prototype.reject = function(rejectReason){
+	if(!((this.$promise.state == 'rejected') || (this.$promise.state == 'resolved'))){
+		this.$promise.state = 'rejected';
+		this.$promise.value = rejectReason;		
+	}
+}
+
+function defer(){
+	var returnDeferral = new Deferral;
+	return returnDeferral;
+}
 
 
+// $Promise.prototype.handler = {
+// 	var successCb;
+// 	var errorCb;
+// }
 
 /*-------------------------------------------------------
 The spec was designed to work with Test'Em, so we don't
